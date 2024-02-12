c689216dc5229274db2f4dbdf57e20d7c66a83d4
Kognogbe <159786691+Kognogbe@users.noreply.github.com>
2024-02-12 18:17:02 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,9 +1,5 @@
-from flask import Flask, render_template_string, render_template, jsonify
-from flask import render_template
-from flask import json
-from datetime import datetime
-from urllib.request import urlopen
-import sqlite3
+from flask import Flask, render_template
+import requests 
 
 app = Flask(__name__)
 
