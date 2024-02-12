86899d33e10640a1900da6e06eaccd31aa5f5de1
Kognogbe <159786691+Kognogbe@users.noreply.github.com>
2024-02-12 18:27:42 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -10,17 +10,6 @@ def contact():
 @app.route('/')
 def hello_world():
     return render_template('hello.html')
-@app.route('/paris/')
-def meteo():
-    response = urlopen('https://api.openweathermap.org/data/2.5/forecast/daily?q=Paris,fr&cnt=16&appid=59738d3765db6902c9ab2d58c9c8b580')
-    raw_content = response.read()
-    json_content = json.loads(raw_content.decode('utf-8'))
-    results = []
-    for list_element in json_content.get('list', []):
-        dt_value = list_element.get('dt')
-        temp_day_value = list_element.get('temp', {}).get('day') - 273.15 # Conversion de Kelvin en °c 
-        results.append({'Jour': dt_value, 'temp': temp_day_value})
-    return jsonify(results=results)
 @app.route('/histogramme/')
 def get_weather_data():
     api_url = 'https://samples.openweathermap.org/data/2.5/forecast?q=Paris,fr&appid=59738d3765db6902c9ab2d58c9c8b580' # Remplacez "xxx" par votre clé d'API OpenWeatherMap
