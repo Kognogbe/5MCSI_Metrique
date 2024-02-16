c8ff0189849dec42ab4c3bbc10989105e7e96dae
Kognogbe <159786691+Kognogbe@users.noreply.github.com>
2024-02-16 14:21:43 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,33 +1,21 @@
-from flask import Flask, render_template
-import requests 
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/contact/")
+@app.route('/contact/', methods=['GET', 'POST'])
 def contact():
+    if request.method == 'POST':
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        message = request.form['message']
+        # Traitez les données du formulaire ici (par exemple, envoyez un email, sauvegardez les données dans une base de données, etc.)
+        # Pour cet exemple, nous allons simplement afficher les données saisies
+        return f"""
+            <h2>Merci {firstname} {lastname} !</h2>
+            <p>Votre message a été envoyé avec succès :</p>
+            <p>{message}</p>
+        """
     return render_template('contact.html')
 
-@app.route('/')
-def hello_world():
-    return render_template('hello.html')
-@app.route('/histogramme/')
-def get_weather_data():
-    api_url = 'https://samples.openweathermap.org/data/2.5/forecast?q=Paris,fr&appid=59738d3765db6902c9ab2d58c9c8b580' # Remplacez "xxx" par votre clé d'API OpenWeatherMap
-    response = requests.get(api_url)
-    
-    if response.status_code == 200:
-        data = response.json()
-        daily_weather = []
-
-        # Filtrer les données pour n'inclure que les températures et les dates du jour
-        for forecast in data['list']:
-            date = forecast['dt_txt'].split(' ')[0]
-            temperature = forecast['main']['temp']
-            daily_weather.append({'date': date, 'temperature': temperature})
-
-        return render_template('histogramme.html', weather_data=daily_weather)
-    else:
-        return 'Erreur lors de la récupération des données météorologiques', 500
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
