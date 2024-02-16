7537605c797e0d720c86b68240b2ff75f08fe3bd
Kognogbe <159786691+Kognogbe@users.noreply.github.com>
2024-02-16 17:08:14 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 def hello_world():
     return render_template('hello.html') #Comm2
 
-@app.route("/contact/")
+@app.route("/Contact/")
 def MaPremiereAPI():
     return render_template("Contact.html")
 
